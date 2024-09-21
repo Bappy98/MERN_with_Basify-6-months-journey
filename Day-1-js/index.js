ae406ab5a73810024a0814c6f96e666f4bbc79c7
Faysal Ahmed Bappy <faysalahmed.ces.98@gmail.com>
2024-09-21 19:56:16 +0600

    js

---
+++ Day-1-js/index.js
@@ -0,0 +1,17 @@
+var number = 10; //integer
+console.log(number);
+var name = 'faysal' //string
+var bool = true //boolean
+
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+
+//The let and const keywords were added to JavaScript in 2015.
+
+//The var keyword should only be used in code written for older browsers.
+
+let a = 10;
+let arr = [1,2,3]; //array
+const obj = {     //object
+    name:'faysal',
+    age:12
+}
